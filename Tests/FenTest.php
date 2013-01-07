aa99b18144f801ccbeb0a39364f6123a295fdec5
DHTMLGoodies <post@dhtmlgoodies.com>
2013-01-07 20:31:46 +0100

    FEN class

--- Tests/FenTest.php
+++ Tests/FenTest.php
@@ -21,4 +21,18 @@ class FenTest extends ChessTests
         // then
         $this->assertNotNull($fen->getId());
     }
+
+    /**
+     * @test
+     */
+    public function shouldNotAutoCreateFenWhenExists(){
+        // given
+        $fen = new Fen("rnbqkbnr\/pppppppp\/8\/8\/8\/8\/PPPPPPPP\/RNBQKBNR w KQkq - 0 1");
+        $fen2 = new Fen("rnbqkbnr\/pppppppp\/8\/8\/8\/8\/PPPPPPPP\/RNBQKBNR w KQkq - 0 1");
+
+        // then
+        $this->assertEquals($fen->getId(), $fen2->getId());
+
+
+    }
 }
