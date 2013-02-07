55f67ccc6a35dc6a8f2b586d5b4b1fcdb7455fc0
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-07 02:17:23 +0100

    Countries table collection

---
+++ player/Countries.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Alf Magne Kalleland
+ * Date: 06.02.13
+ */
+class Countries extends LudoDBCollection implements LudoDBService
+{
+    protected $enableCaching = true;
+    protected $config = array(
+        "sql" => "select * from country order by name"
+    );
+
+    public static function getValidServices(){
+        return array('read');
+    }
+    public function areValidServiceArguments($service, $arguments){
+        return count($arguments) === 0;
+    }
+}
